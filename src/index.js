07701ea83f23e11c65d4b2ff41247f03a826bf5b
iambouamar <iambouamar@gmail.com>
2017-11-15 22:49:24 +0000

    the reducer for add to cart and the store are added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import { createStore } from "redux";
+import store from './store';
+import { addToCart } from './actions/cart-actions';
 
-const reducer = function(state, action) {  
-    return state;
-}
 
-const store = createStore(reducer);
+
+console.log("initial state: ", store.getState());
+
+let unsubscribe = store.subscribe(() =>  
+    console.log(store.getState())
+);
+
+store.dispatch(addToCart('Coffee 500gm', 1, 250));
+store.dispatch(addToCart('Flour 1kg', 2, 110));
+store.dispatch(addToCart('Juice 2L', 1, 250));
+unsubscribe();
